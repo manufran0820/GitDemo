c7d22dc0031e6188ee2ea53c4daf6bb4d1044afa
manufran0820 <franciscogomez@Franciscos-MacBook-Air.local>
2025-02-07 14:01:02 -0800

    firts commit

---
+++ alerts.py
@@ -0,0 +1,17 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+driver = webdriver.Chrome()
+driver.get('https://rahulshettyacademy.com/AutomationPractice/')
+name = 'Rahul'
+driver.find_element(By.CSS_SELECTOR, "#name").send_keys(name)
+driver.find_element(By.ID, "alertbtn").click()
+time.sleep(4)
+alert = driver.switch_to.alert
+alertText = alert.text
+print(alert.text)
+assert name in alertText
+alert.accept()
+
+#alert.dismiss()
+
